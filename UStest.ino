1999a61b32de590867029d89b369db73775663f3
orti121 <orti121@gmail.com>
2018-03-27 10:17:00 -0300

    Add files via upload

---
+++ UStest.ino
@@ -0,0 +1,40 @@
+//Control de sensor de ultrasonido extraído de https://create.arduino.cc/projecthub/josemanu/medir-distancias-con-hc-sr04-63f81e
+
+long tiempo;
+int disparador = 12;   // triger
+int entrada = 13;      // echo
+int supply = 11;      // Alimentación
+float distancia;
+
+void setup()
+{
+  pinMode(disparador, OUTPUT);
+  pinMode(entrada, INPUT);
+  pinMode(supply, OUTPUT);
+  digitalWrite(supply,HIGH);
+  Serial.begin(9600);
+}
+
+
+void loop()
+{
+  // lanzamos un pequeño pulso para activar el sensor
+  digitalWrite(disparador, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(disparador, LOW);
+  
+  // medimos el pulso de respuesta
+  tiempo = (pulseIn(entrada, HIGH)/2); // dividido por 2 por que es el 
+                                       // tiempo que el sonido tarda
+                                       // en ir y en volver
+  // ahora calcularemos la distancia en cm
+  // sabiendo que el espacio es igual a la velocidad por el tiempo
+  // y que la velocidad del sonido es de 343m/s y que el tiempo lo 
+  // tenemos en millonesimas de segundo
+  distancia = float(tiempo * 0.0343);
+  
+  // y lo mostramos por el puerto serie una vez por segundo
+  Serial.println(distancia);
+  delay(20);
+}
+
---
+++ mergetest.py
@@ -0,0 +1,36 @@
+# Merge test
+#https://www.todavianose.com/leer-el-puerto-serie-de-arduino-con-python-y-pyserial/
+#https://matplotlib.org/examples/pylab_examples/polar_demo.html
+
+#!/usr/bin/python
+
+# Importamos la libreira de PySerial
+import serial
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Abrimos el puerto del arduino a 9600
+PuertoSerie = serial.Serial('COM9', 9600)
+# Creamos un buble sin fin
+while True:
+  # leemos hasta que encontarmos el final de linea
+  sArduino = PuertoSerie.readline()
+  # Mostramos el valor leido y eliminamos el salto de linea del final
+  print (sArduino)
+
+
+rstop = 40
+rstart = 0
+r = np.ones(10000)
+size = len(r)
+thstep = 2*np.pi/size
+theta = np.arange(0,2*np.pi,thstep)
+ax = plt.subplot(111, projection='polar')
+ax.plot(theta, r)
+ax.set_rmax(2)
+ax.set_rticks([0.5, 1, 1.5, 2])  # less radial ticks
+ax.set_rlabel_position(-22.5)  # get radial labels away from plotted line
+#ax.grid(True)
+
+ax.set_title("A line plot on a polar axis", va='bottom')
+plt.show()
